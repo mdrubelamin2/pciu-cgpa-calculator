4ebc9db5b3df60f04ad2755a3badcffecfae2f89
mdrubelamin2 <mdrubelamin2@gmail.com>
2022-06-01 11:57:42 +0600

    fixed fetch-count to greater than for interval timeout

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -101,7 +101,7 @@ formElm.addEventListener('submit', (e) => {
   const interval = setInterval(() => {
     const fetchCount = localStorage.getItem('fetch-count')
 
-    if (fetchCount == allTrimesters.length) {
+    if (fetchCount >= allTrimesters.length) {
       // clear the interval
       clearInterval(interval)
 
