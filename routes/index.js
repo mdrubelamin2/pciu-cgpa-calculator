643c5f4ebe120221fac2deecd65fe8ef76c0be02
mdrubelamin2 <mdrubelamin2@gmail.com>
2022-06-15 09:57:12 +0600

    removed: unnecessary codes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 var express = require('express')
 var router = express.Router()
-var fetch = require('node-fetch')
 var HTMLParser = require('node-html-parser')
 var axios = require('axios')
 var FormData = require('form-data')
@@ -16,46 +15,45 @@ router.get('/', function (req, res, next) {
 
 router.get('/get-online-result/:id', async function (req, res, next) {
   const studentId = req.params.id
-  const data = await fetch('http://119.18.149.45/PCIUOnlineResult')
-  const body = await data.text()
-  var root = HTMLParser.parse(body);
+  const resp = await axios.get('http://119.18.149.45/PCIUOnlineResult')
+  const respData = resp.data
+  var root = HTMLParser.parse(respData);
 
   // get the cookies from the fetch response
-  const cookies = data.headers.get('set-cookie')
+  const cookies = respData.headers.get('set-cookie')
   const cookie = cookies.split(';')[0]
 
   const requestVerificationToken = root.querySelector('form').firstChild.getAttribute('value')
   const semester = root.querySelector('#Semester').rawAttributes.Value
 
-  if(!(requestVerificationToken && semester)) return res.json([])
+  if (!(requestVerificationToken && semester)) return res.json([])
 
-    const formData = new FormData();
-    formData.append('__RequestVerificationToken', requestVerificationToken);
-    formData.append('StudentIdNo', studentId);
-    formData.append('Semester', semester);
+  const formData = new FormData();
+  formData.append('__RequestVerificationToken', requestVerificationToken);
+  formData.append('StudentIdNo', studentId);
+  formData.append('Semester', semester);
 
-    var config = {
-      method: 'post',
-      url: 'http://119.18.149.45/PCIUOnlineResult',
-      headers: { 
-        'Cookie': cookie, 
-        ...formData.getHeaders()
-      },
-      data : formData
-    };
+  const url = 'http://119.18.149.45/PCIUOnlineResult'
+  var config = {
+    headers: {
+      'Cookie': cookie,
+      ...formData.getHeaders()
+    },
+    data: formData
+  };
 
-  const resultResp = await axios(config)
-  const bodyHTML = resultResp.data.replace(/[\n\r]+|[\s]{2,}/g, ' ').trim()
+  const resultResp = await axios.post(url, config)
+  const resultData = resultResp.data
+  const bodyHTML = resultData.replace(/[\n\r]+|[\s]{2,}/g, ' ').trim()
   const parsedResHTML = HTMLParser.parse(bodyHTML)
   const cgpaTd = parsedResHTML.querySelector('.table:first-child tr:last-child td:last-child')
 
-  if(!cgpaTd) return res.json([])
-  
+  if (!cgpaTd) return res.json([])
+
   const cgpaTxt = cgpaTd.text
   const GPA = parseFloat(cgpaTxt)
-  
+
   const allTrFromSecondTbodyExceptFirstRow = parsedResHTML.querySelectorAll('.table:nth-child(2) tr:not(:first-child)')
-  // console.log('allTables', allTrFromSecondTbodyExceptFirstRow)
   const results = []
   for (let i = 0; i < allTrFromSecondTbodyExceptFirstRow.length; i++) {
     const tr = allTrFromSecondTbodyExceptFirstRow[i]
@@ -76,11 +74,11 @@ router.get('/get-student-info/:id', async function (req, res, next) {
   const id = req.params.id
   // fetch the student info
   const url = `http://119.18.149.45/StudentAPI/api/studentinfo/get?studentIdNo=${id}`
-  const studentInfo = await fetch(url)
-  const studentInfoJson = await studentInfo.json()
+  const studentInfoJson = await axios.get(url)
+  const studentInfo = studentInfoJson.data
 
   // return the student info
-  res.json(studentInfoJson)
+  res.json(studentInfo)
 })
 
 router.get('/get-trimester-result/:id/:trimester', async function (req, res, next) {
@@ -88,11 +86,11 @@ router.get('/get-trimester-result/:id/:trimester', async function (req, res, nex
   const trimester = req.params.trimester
   // fetch the trimester result
   const url = `http://119.18.149.45/StudentAPI/api/StudentResult/get?studentIdNo=${id}&Trimester=${trimester}`
-  const trimesterResult = await fetch(url)
-  const trimesterResultJson = await trimesterResult.json()
+  const trimesterResultJson = await axios.get(url)
+  const trimesterResult = trimesterResultJson.data
 
   // return the trimester result
-  res.json(trimesterResultJson)
+  res.json(trimesterResult)
 })
 
 
