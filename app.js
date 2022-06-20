3db65881306f23aae8ae6de6de34b4f86f439a16
Md Rubel Amin <mdrubelamin2@gmail.com>
2022-06-20 23:11:35 +0600

    remove: unnecessary all middleware from app

--- app.js
+++ app.js
@@ -1,45 +1,12 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors');
-var compression = require('compression');
-var minify = require('express-minify');
+const express = require('express')
+const path = require('path')
+const indexRouter = require('./route')
+const app = express()
 
-var indexRouter = require('./routes/index');
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'dist')))
 
-var app = express();
+app.use('/', indexRouter)
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(cors())
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(compression());
-app.use(minify());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+module.exports = app
