66fa398df6ec91c8c571fd8c23603f07a263ec35
Md Rubel Amin <mdrubelamin2@gmail.com>
2022-06-21 22:10:36 +0600

    feat: production & development mode separation for better development experience

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const path = require('path')
 const indexRouter = require('./route')
 const app = express()
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(express.static(path.join(__dirname, 'dist')))
+const nodeEnv = (process.env.NODE_ENV).trim()
+const viewFolder = (nodeEnv === 'development') ? 'public' : 'dist'
+
+app.use(express.static(path.join(__dirname, viewFolder)))
 
 app.use('/', indexRouter)
 
