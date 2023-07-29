98e5cdedee2e2683a2513e893e6a942a59bcf625
Md Rubel Amin <mdrubelamin2@gmail.com>
2023-07-29 18:25:30 +0600

    chore: max-age 0 for cache control

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ app.use(express.static(path.join(__dirname, '../', viewFolder), {
     setHeaders: (res, path) => {
         if (express.static.mime.lookup(path) === 'text/html') {
             // Skip cache on html to load new builds.
-            res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
+            res.setHeader("Cache-Control", "max-age=0, s-maxage=0, no-store, no-cache, must-revalidate, proxy-revalidate");
             res.setHeader("Expires", "0");
             res.header('Pragma', 'no-cache');
             res.setHeader("Surrogate-Control", "no-store");
