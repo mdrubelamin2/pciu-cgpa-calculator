d4e784cc5875e94a11caca3c27aac2f7d43688bb
gazirahad7 <65822873+gazirahad7@users.noreply.github.com>
2023-08-01 22:11:59 +0600

    Added: APIs endpoint

---
+++ pciu-cgpa-calculator-v2/app/utils/apisEndpoint.tsx
@@ -0,0 +1,5 @@
+const trimesterResultAPI: string =
+  "http://119.18.149.45/PCIUStudentPortal/Student/TrimesterResult";
+const onlineResultAPI: string = "http://119.18.149.45/PCIUOnlineResult";
+
+export { trimesterResultAPI, onlineResultAPI };
