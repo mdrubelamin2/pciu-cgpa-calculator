0d68427f567c2c3cd78aeca9627ceb8dc7349fcc
Md Rubel Amin <mdrubelamin2@gmail.com>
2023-08-05 18:20:29 +0600

    chore: implemented chartjs
    removed: react-charts

--- src/components/CGPAChart/CGPAChart.jsx
+++ src/components/CGPAChart/CGPAChart.jsx
@@ -2,73 +2,78 @@
 
 import { $allResults } from "@/atoms/global"
 import { getAverageCGPAandCredits, roundToTwoDecimal } from "@/utils/helpers"
+import { CategoryScale, Chart as ChartJS, Legend, LinearScale, LineElement, PointElement, Title, Tooltip } from 'chart.js'
 import { useAtomValue } from "jotai"
 import { useMemo } from "react"
-import { Chart } from "react-charts"
+import { Line } from 'react-chartjs-2'
 import styles from './style.module.css'
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+);
 
 const generateChartData = allResults => {
     const allResultData = [...allResults].reverse()
     const resultData = allResultData.filter(result => result.currentGPA > 0)
-    const data = [
-        {
-            label: "CGPA",
-            data: resultData.map((_, i) => ({
-                primary: resultData[i].trimester,
-                secondary: Number(getAverageCGPAandCredits(resultData, i).totalAverageCGPA),
-            })),
-        },
-        {
-            label: "SGPA",
-            data: resultData.map((result) => ({
-                primary: result.trimester,
-                secondary: roundToTwoDecimal(result.currentGPA),
-            })),
-        }
-    ]
+
+    const data = {
+        labels: resultData.map(result => result.trimester),
+        datasets: [
+            {
+                label: 'SGPA ',
+                backgroundColor: '#00a3ff',
+                borderColor: '#00a3ff',
+                data: resultData.map(result => roundToTwoDecimal(result.currentGPA)),
+            },
+            {
+                label: 'CGPA ',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: resultData.map((_, i) => getAverageCGPAandCredits(resultData, i).totalAverageCGPA),
+            },
+        ]
+    };
 
     return data
+}
+
+const config = {
+    maintainAspectRatio: false,
+    interaction: {
+        mode: 'index',
+        intersect: false,
+    },
+    plugins: {
+        legend: {
+            position: 'top',
+        },
+        title: {
+            display: true,
+            text: 'Chart',
+        },
+    },
 }
 
 export default function CGPAChart() {
     const allResults = useAtomValue($allResults)
 
-    const data = useMemo(
+    const chartOptions = useMemo(
         () => generateChartData(allResults),
         [allResults]
     )
 
-    const primaryAxis = useMemo(
-        () => ({
-            getValue: (datum) => datum.primary,
-        }),
-        []
-    )
-
-    const secondaryAxes = useMemo(
-        () => [
-            {
-                getValue: (datum) => datum.secondary,
-                elementType: 'line',
-            },
-        ],
-        []
-    )
-
-    const isDataExists = data[0].data.length > 0 && data[1].data.length > 0
-
-    if (!isDataExists) return null
+    if (!allResults.length) return null
 
     return (
         <div className={styles.chartContainer}>
             <div className={styles.chartBox}>
-                <Chart
-                    options={{
-                        data,
-                        primaryAxis,
-                        secondaryAxes,
-                    }}
-                />
+                <Line options={config} data={chartOptions} />
             </div>
         </div>
     )
