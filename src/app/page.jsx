ad62dcf79dc146ef2c345b0d6ddede38a151999f
Md Rubel Amin <mdrubelamin2@gmail.com>
2023-08-07 11:59:12 +0600

    misc changes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,14 +3,14 @@
 import { $studentInfo } from '@/atoms/global'
 import Heading from '@/components/Heading/Heading'
 import LeftContainer from '@/components/LeftContainer/LeftContainer'
-import RightContainer from '@/components/RightContainer/RightContainer'
 import { isObjectEmpty } from '@/utils/helpers'
 import { useAtomValue } from 'jotai'
 import dynamic from 'next/dynamic'
 import styles from './page.module.css'
 
-const CGPAChart = dynamic(() => import('@/components/CGPAChart/CGPAChart'), { ssr: false })
-const ResultModal = dynamic(() => import('@/components/ResultModal/ResultModal'), { ssr: false })
+const RightContainer = dynamic(() => import('@/components/RightContainer/RightContainer'))
+const CGPAChart = dynamic(() => import('@/components/CGPAChart/CGPAChart'))
+const ResultModal = dynamic(() => import('@/components/ResultModal/ResultModal'))
 
 export default function Home() {
   const studentInfo = useAtomValue($studentInfo)
