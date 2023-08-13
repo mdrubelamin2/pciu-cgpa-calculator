dbf965a5c88d55f8fe64497b8248780ff4d18398
Arif Uddin <arifunctg@gmail.com>
2023-08-13 06:56:13 +0600

    chore: add oficial logo

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -1,12 +1,16 @@
+import Image from 'next/image'
 import styles from './style.module.css'
 
 export default function Heading() {
     return (
         <div className={styles.headerSection}>
-            <h1 className={styles.headerTitle}>CGPA Calculator</h1>
-            <p className={styles.headerSubtitle}>
-                Port City International University, Chattogram
-            </p>
+            <Image src="/images/pciu-logo.png" alt="pciu logo" width={35} height={46} />
+            <div>
+                <h1 className={styles.headerTitle}>Online Result</h1>
+                <p className={styles.headerSubtitle}>
+                    Port City International University, Chattogram
+                </p>
+            </div>
         </div>
     )
 }
