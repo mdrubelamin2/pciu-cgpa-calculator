c48ac6b810698881fa5b6bb4dac414d8cb3d6528
Md Rubel Amin <mdrubelamin2@gmail.com>
2023-08-21 23:51:47 +0600

    feat: page loader comp

---
+++ src/components/PageLoader/PageLoader.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./style.module.css";
+
+export default function PageLoader() {
+  return (
+    <div className={styles.loadingContainer}>
+      <Image src="/images/pciu-logo.png" alt="pciu logo" width={100} height={120} />
+      <span data-text="Loading…" className={styles.loading}>Loading…</span>
+    </div>
+  )
+}
