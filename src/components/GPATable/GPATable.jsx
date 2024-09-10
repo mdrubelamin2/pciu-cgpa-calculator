684e135ee684e7aa71c5fa563e912204d674e931
Md Rubel Amin <mdrubelamin2@gmail.com>
2024-09-11 00:44:40 +0600

    feat: animate gpa table

--- src/components/GPATable/GPATable.jsx
+++ src/components/GPATable/GPATable.jsx
@@ -5,6 +5,7 @@ import { roundToTwoDecimal } from '@/utils/helpers'
 import { useAtomValue, useSetAtom } from 'jotai'
 import Image from 'next/image'
 import styles from './style.module.css'
+import { MagicMotion } from 'react-magic-motion'
 
 export default function GPATable() {
     const allResults = useAtomValue($allResults)
@@ -23,32 +24,34 @@ export default function GPATable() {
                         <th>Details</th>
                     </tr>
                 </thead>
-                <tbody>
-                    {allResults.map(({ trimester, totalCreditHrs, currentGPA }, indx) => (
-                        <tr key={trimester}>
-                            <td>{trimester}</td>
-                            <td>{totalCreditHrs}</td>
-                            <td>{currentGPA ? roundToTwoDecimal(currentGPA, true) : '0.00'}</td>
-                            <td>
-                                <button className={styles.detailsBtn} onClick={showModal(allResults[indx])}>
-                                    {editMode && (
-                                        <>
-                                            <Image src="/images/edit.svg" className="edit-img" width={13} height={13} alt="Edit" />
-                                            <span>Edit</span>
-                                        </>
-                                    )}
-                                    {!editMode && (
-                                        <>
-                                            <Image src="/images/info.svg" className="info-img" width={13} height={13} alt="Info" />
-                                            <span>Details</span>
-                                        </>
-                                    )}
-                                </button>
-                            </td>
-                        </tr>
-                    ))}
+                <MagicMotion layoutDependency={allResults}>
+                    <tbody>
+                        {allResults.map(({ trimester, totalCreditHrs, currentGPA }, indx) => (
+                            <tr key={trimester}>
+                                <td>{trimester}</td>
+                                <td>{totalCreditHrs}</td>
+                                <td>{currentGPA ? roundToTwoDecimal(currentGPA, true) : '0.00'}</td>
+                                <td>
+                                    <button className={styles.detailsBtn} onClick={showModal(allResults[indx])}>
+                                        {editMode && (
+                                            <>
+                                                <Image src="/images/edit.svg" className="edit-img" width={13} height={13} alt="Edit" />
+                                                <span>Edit</span>
+                                            </>
+                                        )}
+                                        {!editMode && (
+                                            <>
+                                                <Image src="/images/info.svg" className="info-img" width={13} height={13} alt="Info" />
+                                                <span>Details</span>
+                                            </>
+                                        )}
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
 
-                </tbody>
+                    </tbody>
+                </MagicMotion>
             </table>
         </div>
     )
